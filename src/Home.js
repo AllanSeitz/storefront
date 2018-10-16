266ba76974f8bd928405a5df8dfff76eb66faacf
Allan <allan@Allans-MacBook-Pro.local>
2018-10-16 17:31:06 -0400

    working on bootstrap

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,28 @@ import Data from './Data.json'
 class Home extends Component {
   render() {
     return (
-      <main>
-        <h1>Featured ITems</h1>
-        {Object.keys(Data).map(category => {
-          return (
-            <aside className="gear">
-              <p className="text">{Data[category].description}</p>
-              <img
-                className="main-image"
-                src={Data[category].photos[0].imageURL}
-              />
-            </aside>
-          )
-        })}
-      </main>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xs-12 col-md-8">
+            <h1>Featured Items</h1>
+            {Object.keys(Data).map(category => {
+              return (
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    {Data[category].description}
+                  </li>
+                  <li className="list-group-item">
+                    <img
+                      alt="fishing gear"
+                      src={Data[category].photos[0].imageURL}
+                    />
+                  </li>
+                </ul>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     )
   }
 }
